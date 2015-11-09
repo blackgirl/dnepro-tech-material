e685fa91c768ec5aa9f87c457854660e1baee053
Alyona <a.chornaya@gmail.com>
2015-11-09 17:57:05 +0200

    Re designed & filled with original text
    All without Mixins page

--- js/fixed-nav.js
+++ js/fixed-nav.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
 /**
  * This object controls the nav bar. Implement the add and remove
  * action over the elements of the nav bar that we want to change.
@@ -7,15 +6,11 @@ $(document).ready(function(){
  * @type {{flagAdd: boolean, elements: string[], add: Function, remove: Function}}
  */
 var myNavBar = {
-
     flagAdd: true,
-
     elements: [],
-
     init: function (elements) {
         this.elements = elements;
     },
-
     add : function() {
         if(this.flagAdd) {
             for(var i=0; i < this.elements.length; i++) {
@@ -24,7 +19,6 @@ var myNavBar = {
             this.flagAdd = false;
         }
     },
-
     remove: function() {
         for(var i=0; i < this.elements.length; i++) {
             document.getElementById(this.elements[i]).className =
@@ -32,9 +26,7 @@ var myNavBar = {
         }
         this.flagAdd = true;
     }
-
 };
-
 /**
  * Init the object. Pass the object the array of elements
  * that we want to change when the scroll goes down
@@ -44,32 +36,26 @@ myNavBar.init(  [
     "header-container",
     "brand"
 ]);
-
 /**
  * Function that manage the direction
  * of the scroll
  */
 function offSetManager(){
-
-    var yOffset = 0;
+    var yOffset = 150;
     var currYOffSet = window.pageYOffset;
-
     if(yOffset < currYOffSet) {
         myNavBar.add();
     }
-    else if(currYOffSet == yOffset){
+    else if(currYOffSet == 0){
         myNavBar.remove();
     }
-
 }
-
 /**
  * bind to the document scroll detection
  */
 window.onscroll = function(e) {
     offSetManager();
 }
-
 /**
  * We have to do a first detectation of offset because the page
  * could be load with scroll down set.
